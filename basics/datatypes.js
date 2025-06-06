e26ddaebbf0bd3c4484013d3287ad243168f8a7e
Saniya-63 <saniyashinde6301@gmail.com>
2025-06-06 15:45:56 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version 
+
+//alert(3+3)    //we are using nodejs ,not browser so its not allowed here 
+
+
+let name = "saniya"
+let age = 24
+let isLoggedIn = false
+
+//primitive -datatypes
+//number => 2 to power 53
+//bigint
+//string => " "
+//boolean => true/False
+//null =>standalone value 
+//undefined => value is not assigned 
+//symbol => unique 
+
+//object
+console.log(typeof undefined);  //undefined
+console.log(typeof null); //object
