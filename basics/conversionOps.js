77819ddd0b7b626c8ccc2f18ad7d8939377c5e58
Saniya-63 <saniyashinde6301@gmail.com>
2025-06-07 12:50:53 +0000

    operations

--- basics/conversionOps.js
+++ basics/conversionOps.js
@@ -1,7 +1,7 @@
-let score = "saniya"
-// Converting a string to a number
-console.log(typeof score);
-console.log(typeof(score));
+// let score = "saniya"
+// // Converting a string to a number
+// console.log(typeof score);
+// console.log(typeof(score));
 
 // Using Number() to convert a string to a number
 //let isLoggedIn = 1
@@ -9,20 +9,58 @@ console.log(typeof(score));
 //console.log(boleanIsLoggedIn);
 
 // Using Number() to convert a string to a number
-let valueINNumber = Number(score)
-console.log(typeof valueINNumber);
-console.log(valueINNumber);
+// let valueINNumber = Number(score)
+// console.log(typeof valueINNumber);
+// console.log(valueINNumber);
 
 // Converting a number to a string
 //33 =>33
 //"33abc" => NaN
 //true => 1; False=>0
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 // Converting a boolean to a string
-let isLoggedIn = "saniya"
- let boleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(boleanIsLoggedIn); // true
+// let isLoggedIn = "saniya"
+//  let boleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boleanIsLoggedIn); // true
+
+//**************Operations*************
+
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue); // -3
+
+//arithmetic operations
+// console.log(3 + 4); // 7
+// console.log(3 - 4); // -1
+// console.log(3 * 4); // 12
+// console.log(3 / 4); // 0.75
+// console.log(3 % 4); // 3
+// console.log(3 ** 4); // 81
+
+let str1 = "hello"
+let str2 = "Saniya"
+// string concatenation
+let str3 = str1  + str2 // console.log(str3); // helloSaniya
+
+//concatenation with numbers
+// console.log("1" +2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + "2"); //12
+// console.log("1" + 2 + 3); //123
+// console.log(1 + 2 + "3"); //33
+
+
+// console.log(+true); // 1
+// console.log(+false); // 0
+// console.log(+""); // 0
+// console.log(+"saniya"); // NaN
+
+let num1,num2,num3
+num1=num2=num3 = 2 + 2
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter); // 101
