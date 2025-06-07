1d980f13e80a977c281c993571723ae3556338e9
Saniya-63 <saniyashinde6301@gmail.com>
2025-06-07 12:28:55 +0000

    conversion of dataypes

---
+++ basics/conversionOps.js
@@ -0,0 +1,28 @@
+let score = "saniya"
+// Converting a string to a number
+console.log(typeof score);
+console.log(typeof(score));
+
+// Using Number() to convert a string to a number
+//let isLoggedIn = 1
+//let boleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(boleanIsLoggedIn);
+
+// Using Number() to convert a string to a number
+let valueINNumber = Number(score)
+console.log(typeof valueINNumber);
+console.log(valueINNumber);
+
+// Converting a number to a string
+//33 =>33
+//"33abc" => NaN
+//true => 1; False=>0
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// Converting a boolean to a string
+let isLoggedIn = "saniya"
+ let boleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(boleanIsLoggedIn); // true
