e5ca9722fd1a5cc729a30f89d7495b92ab706a1d
Saniya-63 <saniyashinde6301@gmail.com>
2025-06-18 07:24:28 +0000

    object  literals

--- basics_02/objects.js
+++ basics_02/objects.js
@@ -1,1 +1,35 @@
-//singleton 
+//literal
+const mysym = Symbol("key1") 
+
+
+jUser ={
+    name: "saniya",
+    [mysym]:"myKey1",
+    age:18,
+    location:"Pune",
+    email:"saniya@gmail.com",
+    isLoggedIn:false,
+    lastLoginDays: ["Monday","saturday"]
+}
+
+// console.log(jUser.email);
+// console.log(jUser["email"]);
+// console.log(jUser[mysym]);
+
+jUser.email ="hitesh@chatgpt.com"
+// Object.freeze(jUser)
+jUser.email ="hitesh@ms.com"
+console.log(jUser);
+
+jUser.greeting = function(){
+    console.log("Hello js user");
+    
+}
+
+jUser.greetingTwo = function(){
+    console.log(`Hello js user, ${this.name}`);
+    
+}
+console.log(jUser.greeting());
+console.log(jUser.greetingTwo());
+
