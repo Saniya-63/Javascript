d094882a9b7deed7fb0209ead13e8f646bc42c1e
Saniya-63 <saniyashinde6301@gmail.com>
2025-06-27 10:56:10 +0000

    while loop

---
+++ loops/while.js
@@ -0,0 +1,32 @@
+//while loop
+
+// while (condition) {
+     
+// }
+
+// let index =0
+// while (index <=10) {
+//     console.log(`value of index is ${index}`);
+//     index = index+2
+// }
+
+// let myArray =["flash","superman","batman"]
+// let arr = 0
+// while (arr<myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr++
+// }
+  
+
+//do-while
+// do {
+    
+// } while (condition);
+
+
+// let score =11
+// do {
+//     console.log("score is 11");
+    
+// } while (score <=10);
+
